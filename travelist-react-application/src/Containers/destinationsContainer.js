291c00e9bf6c0202f189c101f27237b9037d9611
github username <github email address>
2019-06-11 19:43:18 -0500

    updated destinationsContainer with mapping

--- travelist-react-application/src/Containers/destinationsContainer.js
+++ travelist-react-application/src/Containers/destinationsContainer.js
@@ -1,4 +1,19 @@
 import React, { Component } from 'react';
-import RestaurantInput from '../components/restaurants/RestaurantInput'
-import Restaurants from '../components/restaurants/Restaurants'
+import DestinationInput from '../components/destinations/DestinationInput'
+import Destinations from '../components/destinations/Destinations'
 import { connect } from 'react-redux'
+
+class DestinationsContainer extends Component {
+  render() {
+
+  }
+}
+
+const mapStateToProps = state => ({ destinations: state.destinations })
+
+const mapDispatchToProps = dispatch => ({
+  addDestination: text => dispatch({ type: 'ADD_DESTINATION', text }),
+  deleteDestination: id => dispatch({ type: 'DELETE_DESTINATION', id })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(DestinationsContainer)
