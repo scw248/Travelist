66620c0076ad533ce35bb82772603984c5c56ce0
github username <github email address>
2019-06-11 19:39:15 -0500

    created destinationsContainer

---
+++ travelist-react-application/src/Containers/destinationsContainer.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import RestaurantInput from '../components/restaurants/RestaurantInput'
+import Restaurants from '../components/restaurants/Restaurants'
+import { connect } from 'react-redux'
