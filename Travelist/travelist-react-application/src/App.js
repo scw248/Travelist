0612f94089e2c20b5cf23ba0606e7e33158fc893
github username <github email address>
2019-06-13 19:56:13 -0500

    removed ComponentDidMount

--- Travelist/travelist-react-application/src/App.js
+++ Travelist/travelist-react-application/src/App.js
@@ -3,11 +3,6 @@ import DestinationsContainer from './containers/DestinationsContainer';
 
 class App extends Component {
 
-  componentDidMount() {
-    fetch("http://localhost:3000/api/v1/users/1")
-      .then(r => r.json())
-      .then(console.log())
-  }
   render() {
     return (
       <div className="App">
