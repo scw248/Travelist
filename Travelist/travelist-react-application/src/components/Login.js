66d8f7dcd01cc8dc6e0866fe49e4907385b34fd0
github username <github email address>
2019-06-14 13:35:40 -0500

    created login component and built form

---
+++ Travelist/travelist-react-application/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <form onSubmit={}>
+      <input type="text" name="email" onChange={} value={}/>
+      <input type="text" name="password" onChange={} value={}/>
+    </form>
+  )
+}
+
+export default Login
