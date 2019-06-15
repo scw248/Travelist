4ec6b2f49ddcbbba3ac4f0ff53d2d1fc1a23ff09
github username <github email address>
2019-06-15 17:22:42 -0500

    added logout method in currentUser actions and reducer

--- Travelist/travelist-react-application/src/actions/currentUserActions.js
+++ Travelist/travelist-react-application/src/actions/currentUserActions.js
@@ -6,6 +6,12 @@ export const setCurrentUser = user => {
   }
 }
 
+export const clearCurrentUser = () => {
+  return {
+    type: "CLEAR_CURRENT_USER"
+  }
+}
+
 
 //asynchronous action creators
 export const login = credentials => {
@@ -28,6 +34,16 @@ export const login = credentials => {
         }
       })
       .catch(console.log)
+  }
+}
+
+export const logout = () => {
+  return dispatch => {
+    dispatch(clearCurrentUser())
+    return fetch('http://localhost:3000/api/v1/logout', {
+      credentials: "include",
+      method: "DELETE"
+    })
   }
 }
 
