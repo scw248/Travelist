54c1192b7ed66e18d68131f10251f17d3beb3b14
github username <github email address>
2019-06-16 18:04:43 -0500

    cleaning up comments

--- Travelist/travelist-react-application/src/store.js
+++ Travelist/travelist-react-application/src/store.js
@@ -4,15 +4,12 @@ import currentUserReducer from './reducers/currentUserReducer'
 import loginFormReducer from './reducers/loginFormReducer'
 import thunk from 'redux-thunk'
 import destinationsReducer from './reducers/destinationsReducer.js';
-// import destinationsReducer from './destinationsReducer'
 
 const reducer = combineReducers({
   users: usersReducer,
   currentUser: currentUserReducer,
   loginForm: loginFormReducer,
   destinations: destinationsReducer
-
-  // destinations: destinationsReducer,
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
