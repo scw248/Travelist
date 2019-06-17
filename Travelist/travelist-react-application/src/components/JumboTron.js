afddc2c56fc3148d88dbc4ab9a1ad406b3807c74
github username <github email address>
2019-06-17 15:18:00 -0500

    created JumboTron bootstrap component

---
+++ Travelist/travelist-react-application/src/components/JumboTron.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { JumboTron as Jumbo, Container } from 'react-bootstrap'
+import styled from 'styled-components'
+import travelImage from '../assets/travelImage.jpg'
