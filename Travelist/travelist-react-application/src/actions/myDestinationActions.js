1c60c0ae9499ab7bcfd458ab0d972252c0afdf19
github username <github email address>
2019-06-18 10:16:48 -0500

    built myDestinations action file for fetching

---
+++ Travelist/travelist-react-application/src/actions/myDestinationActions.js
@@ -0,0 +1,30 @@
+// synchronous actions
+export const setMyDestinations = destinations => {
+  return {
+    type: "SET_MY_DESTINATIONS",
+    destinations
+  }
+}
+
+// async actions
+export const getMyDestinations = () => {
+  return dispatch => {
+    return fetch("http://localhost:3000/api/v1/users/{id}/destinations", {
+      credentials: "include",
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          console.log(response.data)
+          dispatch(setMyDestinations(response.data))
+        }
+      })
+      .catch(console.log)
+  }
+}
