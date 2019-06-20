d0ed8d8bcde8324f6027c55f72319dd9cbbc9cd2
github username <github email address>
2019-06-20 10:02:58 -0500

    updated myDestinations async actions for add and delete

--- Travelist/travelist-react-application/src/actions/myDestinationActions.js
+++ Travelist/travelist-react-application/src/actions/myDestinationActions.js
@@ -44,3 +44,54 @@ export const getMyDestinations = () => {
 }
 
 
+export const newDestination = data => {
+  return dispatch => {
+    const destinationInfo = {
+      data: data
+    }
+    return fetch(`http://localhost:3000/api/v1/users/:userId/destinations`, {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(destinationInfo)
+    })
+      .then(r => r.json())
+      .then(response => {
+        if (response.error) {
+          alert(response.error)
+        } else {
+          dispatch(addDestination(response))
+        }
+      })
+      .catch(console.log)
+  }
+}
+
+export const deleteHome = id => {
+  return dispatch => {
+    return fetch(`http://localhost:3000/api/v1/users/:userId/destinations/:destinationId`, {
+      credentials: "include",
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(id)
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          alert(data.error)
+        } else {
+          dispatch(removeDestination(id))
+        }
+      })
+  }
+}
+
+
+
+
+
