37796c06e400b48f1c1d9ff1ef27af95f4b3a73b
github username <github email address>
2019-07-10 15:13:40 -0500

    add pin and remove pin actions added

--- Travelist/travelist-react-application/src/reducers/destinationsReducer.js
+++ Travelist/travelist-react-application/src/reducers/destinationsReducer.js
@@ -6,6 +6,10 @@ export default (state = [], action) => {
       return [...state, action.destination]
     case "REMOVE_DESTINATION":
       return state.filter(destination => destination.id !== action.id);
+    case "ADD_PIN":
+      return [...state, action.pinned_destination]
+    case "REMOVE_PIN":
+      return state.filter(pin => pin.destination_id !== action.id)
     default:
       return state
   }
