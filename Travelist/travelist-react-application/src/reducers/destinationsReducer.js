9d889dc2639c4d0b048f59fe5b47f868832862f2
stephen wright <scw248@gmail.com>
2019-07-16 16:30:19 -0500

    removed pinned destination actions

--- Travelist/travelist-react-application/src/reducers/destinationsReducer.js
+++ Travelist/travelist-react-application/src/reducers/destinationsReducer.js
@@ -6,12 +6,6 @@ export default (state = [], action) => {
       return [...state, action.destination]
     case "REMOVE_DESTINATION":
       return state.filter(destination => destination.id !== action.id);
-    case "SET_PINNED_DESTINATIONS":
-      return action.pinned_destinations
-    case "ADD_PINNED_DESTINATION":
-      return [...state, action.pinned_destination]
-    case "REMOVE_PINNNED DESTINATION":
-      return state.filter(pinned_destination => pinned_destination.id !== action.id)
     default:
       return state
   }
