dd348061f77b972c64078d0e21ef236e73bf8bd5
stephen wright <scw248@gmail.com>
2019-07-16 16:28:58 -0500

    removed pinned destination action creators

--- Travelist/travelist-react-application/src/actions/destinationActions.js
+++ Travelist/travelist-react-application/src/actions/destinationActions.js
@@ -13,28 +13,7 @@ export const removeDestination = id => {
   }
 }
 
-export const setPinnedDestinations = pinned_destinations => {
-  return {
-    type: 'SET_PINNED_DESTINATIONS',
-    pinned_destinations
-  }
-}
-
-export const newPinnedDestination = pinned_destination => {
-  return {
-    type: "NEW_DESTINATION",
-    pinned_destination
-  }
-}
-
-export const removePinnedDestination = pinned_destination_id => {
-  return {
-    type: "REMOVE_PINNED_DESTINATION",
-    pinned_destination_id
-  }
-}
-
-//asynchronous action creators - PICK UP HERE TO SEE WHERE I'M FETCHING PINNED_DESTINATIONS FROM
+//asynchronous action creators
 export const getDestinations = () => {
   return dispatch => {
     return fetch("http://localhost:3000/api/v1/destinations", {
@@ -79,72 +58,3 @@ export const deleteDestination = (id, currentUser) => {
   }
 }
 
-export const getPinnedDestinations = () => {
-  return dispatch => {
-    return fetch("http://localhost:3000/api/v1/pins", {
-      credentials: "include",
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-      .then(r => r.json())
-      .then(response => {
-        if (response.error) {
-          alert(response.error)
-        } else {
-          console.log(response.data)
-          dispatch(setPinnedDestinations(response.data))
-        }
-      })
-      .catch(console.log)
-  }
-}
-
-export const deletePinnedDestination = (id, currentUser) => {
-  return dispatch => {
-    return fetch(`http://localhost:3000/api/v1/users/${currentUser.id}/pins/${id}`, {
-      credentials: "include",
-      method: "DELETE",
-      headers: {
-        "content-type": "application/json",
-        Accept: "application/json"
-      },
-      body: JSON.stringify(id)
-    })
-      .then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          alert(data.error)
-        } else {
-          dispatch(removePinnedDestination(id))
-        }
-      })
-  }
-}
-
-//THIS IS NOT GOING TO WORK CORRECTLY BECAUSE WE'RE NOT GRABBING FORM DATA
-export const addPinnedDestination = (formData, currentUser) => {
-  return dispatch => {
-    const destinationInfo = {
-      ...formData
-    }
-    return fetch(`http://localhost:3000/api/v1/${currentUser.id}/pins`, {
-      credentials: "include",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(destinationInfo)
-    })
-      .then(r => r.json())
-      .then(response => {
-        if (response.error) {
-          alert(response.error)
-        } else {
-          dispatch(newPinnedDestination(response.data))
-        }
-      })
-      .catch(console.log)
-  }
-}
